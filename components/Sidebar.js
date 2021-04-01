5fe55f8f3f94a0f438c07e5f7cce86696a24ddfe
Rezuan Kassim <rezuankassim@hotmail.com>
2021-04-01 10:14:40 +0800

    added vscode directory & login page and added small div when click profile pic

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,10 +4,13 @@ import Logo from "@/components/icons/Logo";
 import Sun from "@/components/icons/Sun";
 import { useTheme } from "next-themes";
 import { useState, useEffect } from "react";
+import { Transition } from "@headlessui/react";
+import PrimaryButton from "./PrimaryButton";
 
 export default function Sidebar() {
   const [isMounted, setIsMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     setIsMounted(true);
@@ -35,13 +38,33 @@ export default function Sidebar() {
 
         <div className="w-full h-px bg-theme-grayer"></div>
 
-        <div className="relative w-10 h-10 rounded-full">
-          <Image
-            src="/assets/image-avatar.jpg"
-            layout="fill"
-            objectFit="contain"
-            className="rounded-full"
-          />
+        <div
+          className="relative w-full flex items-center justify-center"
+          onClick={() => setShow(!show)}
+        >
+          <div className="cursor-pointer relative w-10 h-10 rounded-full">
+            <Image
+              src="/assets/image-avatar.jpg"
+              layout="fill"
+              objectFit="contain"
+              className="z-20 rounded-full"
+            />
+          </div>
+
+          <Transition
+            show={show}
+            enter="transition ease-in-out duration-500"
+            enterFrom="opacity-30"
+            enterTo="opacity-100"
+            leave="transition ease-in-out duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-30"
+            className="absolute inset-x-0 ml-7"
+          >
+            <div className="flex items-center justify-end w-72 h-12 pl-10 pr-1 py-2 bg-white shadow-theme-lg rounded-full">
+              <PrimaryButton padding="px-5 py-3">Login</PrimaryButton>
+            </div>
+          </Transition>
         </div>
       </div>
     </aside>
